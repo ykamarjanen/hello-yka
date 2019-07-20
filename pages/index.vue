04bb4b13ba473355621af485090f3fa8b9471613
ykamarjanen <yka.marjanen@gmail.com>
2019-07-20 15:15:25 +0300

    New commit

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         Ykä
         <span class="green">Marjanen</span>
       </h1>
-      <h2 class="subtitle">Starter for CodeSandBox</h2>
+      <h2 class="subtitle">What is this</h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation
           <IconLink/>
